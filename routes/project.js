d72f01434cde87ff243a2a8865108f9c7c02d29d
Nailek <nailek_95@live.com>
2017-10-27 00:05:31 +0200

    Projects add FindBy Theme, Description, City, Address. (Not exact value search)

--- routes/project.js
+++ routes/project.js
@@ -23,10 +23,10 @@ module.exports = function(app){
 		Project.findById(req.params._id, function(err, project){
 			if(err)
 				res.status(500).send('Internal Server Error');
-			else if(!project)
+			if(!project)
 				res.status(404).send('Project not found.');
 			else
-				res.send(project);
+				res.status(200).send(project);
 		});
 	}
 
@@ -37,13 +37,84 @@ module.exports = function(app){
 		}
 
 		var regex = new RegExp(req.params.name, 'i');  // 'i' makes it case insensitive
-		Project.find({'name':regex}, function(err, project){
+		Project.find({name:regex}, function(err, project){
 			if(err)
 				res.status(500).send('Internal Server Error');
-			else
-				res.send(project);
+			else if (project && project.length != 0)
+				res.status(200).send(project);
+			else 
+				res.status(404).send("Project not found");
 		});
 	}
+
+	findProjectByTheme = function(req, res){
+		if(!req.params.theme){
+			res.status(400).send('theme required');
+			return;
+		}
+
+		var regex = new RegExp(req.params.theme, 'i');  // 'i' makes it case insensitive
+		Project.find({theme:regex}, function(err, project){
+			if(err)
+				res.status(500).send('Internal Server Error');
+			else if (project && project.length != 0)
+				res.status(200).send(project);
+			else 
+				res.status(404).send("Project not found");
+		});
+	}
+
+	findProjectByDescription = function(req, res){
+		if(!req.params.description){
+			res.status(400).send('description required');
+			return;
+		}
+
+		var regex = new RegExp(req.params.description, 'i');  // 'i' makes it case insensitive
+		Project.find({description:regex}, function(err, project){
+			if(err)
+				res.status(500).send('Internal Server Error');
+			else if (project && project.length != 0)
+				res.status(200).send(project);
+			else 
+				res.status(404).send("Project not found");
+		});
+	}
+
+	findProjectByCity = function(req, res){
+		if(!req.params.city){
+			res.status(400).send('city required');
+			return;
+		}
+
+		var regex = new RegExp(req.params.city, 'i');  // 'i' makes it case insensitive
+		Project.find({city:regex}, function(err, project){
+			if(err)
+				res.status(500).send('Internal Server Error');
+			else if (project && project.length != 0)
+				res.status(200).send(project);
+			else 
+				res.status(404).send("Project not found");
+		});
+	}
+
+	findProjectByAddress = function(req, res){
+		if(!req.params.address){
+			res.status(400).send('address required');
+			return;
+		}
+
+		var regex = new RegExp(req.params.address, 'i');  // 'i' makes it case insensitive
+		Project.find({address:regex}, function(err, project){
+			if(err)
+				res.status(500).send('Internal Server Error');
+			else if (project && project.length != 0)
+				res.status(200).send(project);
+			else 
+				res.status(404).send("Project not found");
+		});
+	}
+
 
 	addProject = function(req,res){
 		if(!req.body.name){
@@ -136,8 +207,11 @@ module.exports = function(app){
 	app.get('/project/findAll', findAllProjects);
 	//returns all the paraments
 	app.get('/project/findByID/:_id', findProjectByID);
-
 	app.get('/project/findByName/:name', findProjectByName);
+	app.get('/project/findByTheme/:theme', findProjectByTheme);
+	app.get('/project/findByDescription/:description', findProjectByDescription);
+	app.get('/project/findByCity/:city', findProjectByCity);
+	app.get('/project/findByAddress/:address', findProjectByAddress);
 	//need to pass name, username, password and email
 	app.post('/project/add', addProject);
 
