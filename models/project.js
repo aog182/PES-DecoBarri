8131dcc155268a1154ddac89ddd47fe8f77ba811
albertLuth <albert.masip.vela@est.fib.upc.edu>
2017-10-31 10:28:58 +0100

    ADD /project/addNote/:_id

--- models/project.js
+++ models/project.js
@@ -5,7 +5,13 @@ var Project = new mongoose.Schema({
 	theme: {type : String, required : false},
 	description: {type : String, required : false},
 	city: {type : String, required : true},
-	address: {type : String, required : false}
+	address: {type : String, required : false},
+	notes : [{
+			title : String, 
+			description: String,
+			author: String, 
+			date: String, 
+		    modifiable: Boolean}]
 });
 
 module.exports = mongoose.model('Project', Project);
--- routes/project.js
+++ routes/project.js
@@ -4,6 +4,7 @@ module.exports = function(app){
 	
 	var db = require('../database/db');
 	var Project = db.model('Project');
+	var User = db.model('User');
 
 	findAllProjects = function(req, res){
 		Project.find({},function(err, projects){
@@ -203,6 +204,61 @@ module.exports = function(app){
 		});
 	}
 
+	addNote = function(req, res){
+		if(!req.params._id){
+			res.status(400).send('_id required');
+			return;
+		}
+		if(!req.body.title){
+			res.status(400).send('title required');
+			return;
+		}
+		if(!req.body.description){
+			res.status(400).send('description required');
+			return;
+		}
+		if(!req.body.author){
+			res.status(400).send('author required');
+			return;
+		}
+		if(!req.body.modifiable){
+			res.status(400).send('modifiable required');
+			return;
+		}
+
+		User.findById(req.body.author,function(err, project){
+			if(err){
+				res.status(500).send('Internal Server Error');
+			}
+			else if(!user){
+				res.status(404).send('User not found');
+			}
+			else{
+				Project.findById(req.params._id, function(err, project){
+					if(err)
+						res.status(500).send('Internal Server Error');
+					else if(!project)
+						res.status(404).send('Project not found.');			
+					else{
+						var date = new Date();
+						var note = {'title':req.body.title, 
+									'description':req.body.title,
+									'author':req.body.author,
+									'modifiable': req.body.modifiable,
+									'date': date};
+						project.notes.push(note);
+						project.save(function(err){
+							if(err)
+								res.status(500).send('Internal Server Error');
+							else
+								res.status(200).send('Project modified');
+						});
+					}
+				});
+			}
+		});
+	};
+
 	//returns all the paraments of all projects
 	app.get('/project/findAll', findAllProjects);
 	//returns all the paraments
@@ -218,5 +274,6 @@ module.exports = function(app){
 	app.put('/project/edit/:_id', editProject);
 
 	app.delete('/project/delete/:_id', deleteProject);
+	app.post('/project/addNote/:_id', addNote);
 
 }
